98b07a09e666ea3f59e477e264c084ae0a1d12df
AhshanHabib26 <habib.net27@gmail.com>
2022-04-30 00:50:56 +0600

    Header Componnent Added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import Home from './Componnents/Pages/Home/Home';
+import Header from './Componnents/Pages/Header/Header';
+import Blog from './Componnents/Pages/Blog/Blog';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header/>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/home' element={<Home/>}/>
+        <Route path='/blog' element={<Blog/>}/>
+      </Routes>
     </div>
   );
 }
---
+++ src/Componnents/Pages/Header/Header.js
@@ -0,0 +1,46 @@
+import React from "react";
+import {
+  Button,
+  Container,
+  Form,
+  FormControl,
+  Nav,
+  Navbar,
+} from "react-bootstrap";
+import { Link } from "react-router-dom";
+import './Header.css'
+
+const Header = () => {
+  return (
+    <div className="container">
+      <Navbar style={{sticky: "top"}} bg="body" expand="lg">
+        <Container fluid>
+          <Navbar.Brand href="#">Car<span className="logo_text">House</span> </Navbar.Brand>
+          <Navbar.Toggle aria-controls="navbarScroll" />
+          <Navbar.Collapse id="navbarScroll">
+            <Nav
+              className="mx-auto  my-2 my-lg-0"
+              style={{ maxHeight: "300px" }}
+              navbarScroll
+            >
+              <Nav.Link as={Link} to='/' >Home</Nav.Link>
+              <Nav.Link as={Link} to='/blog'>Blog</Nav.Link>
+            </Nav>
+
+            <Form className="d-flex">
+              <FormControl
+                type="search"
+                placeholder="Search"
+                className="me-2"
+                aria-label="Search"
+              />
+              <Button variant="outline-success">Search</Button>
+            </Form>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </div>
+  );
+};
+
+export default Header;
